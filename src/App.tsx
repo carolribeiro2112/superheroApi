5e61c5fdd291e97069e1f92d5bd6bcee663b6f75
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2021-03-22 12:32:31 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import Home from './pages/Home';
+
 import GlobalStyle from './styles/global';
 import store from './store';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <Home/>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
       </Provider>
       <GlobalStyle/>
     </div>
